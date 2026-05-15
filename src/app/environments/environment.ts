19b45e16fd4ef5f29a5bb864d30504322d496e1a
Superinka <msolis@superinka.com>
2026-05-15 12:01:13 -0500

    Listo

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://iot-backend-4ug1.onrender.com',
-  wsUrl: 'wss://iot-backend-4ug1.onrender.com/ws'
+  apiUrl: 'https://iot-backend-bmiv.onrender.com',
+  wsUrl: 'wss://iot-backend-bmiv.onrender.com/ws'
 };
