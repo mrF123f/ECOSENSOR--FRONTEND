7428549b013dd2a5ddde06dd76f9a80697eb1a4f
jose <josealejadrohinojosa@gmail.com>
2026-05-27 23:20:19 -0500

    1234

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -18,8 +18,8 @@ export const appConfig: ApplicationConfig = {
 
     //AUTH0 CONFIG
     provideAuth0({
-      domain: 'dev-6u1q0s2nx3pub4do.us.auth0.com',
-      clientId: '6E6cqN5AhiK1oIEWD8p1FUlqrYc4TyX4',
+      domain: 'dev-khvop4d61s5ip8d3.us.auth0.com',
+      clientId: 'TGa8Wkq9JDBkcgYOzMIZbOZ2LVzrpI94',
 
       authorizationParams: {
         redirect_uri:`${window.location.origin}/callback`,
--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://iot-backend-4ug1.onrender.com',    // La URL que te dio Render
-  wsUrl:  'wss://iot-backend-4ug1.onrender.com/ws'
+  apiUrl: 'https://iot-backend-bmiv.onrender.com',    // La URL que te dio Render
+  wsUrl:  'wss://iot-backend-bmiv.onrender.com/ws'
 };
